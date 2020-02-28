1b9a8baafcb15faeb1ffaf882fdebe46b7e96470
jaketrent <trent.jake@gmail.com>
2020-02-28 10:45:47 -0700

    styled components version

--- play/src/App.js
+++ play/src/App.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 import './App.css'
 import Newsletter from './Newsletter.js'
+import NewsletterStyledComponent from './NewsletterStyledComponent.js'
 import * as css from './NewsletterOverride.module.css'
 
 function App() {
   return (
     <div className="App">
-      <p>Before para</p>
+      <p>CSS mod</p>
       <div className="App-newsletter">
         <Newsletter theme={css} />
       </div>
-      <p>After para</p>
+      <p>Styled</p>
+      <div className="App-newsletter">
+        <NewsletterStyledComponent />
+      </div>
     </div>
   )
 }
