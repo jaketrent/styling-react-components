8cc0dccadf6240719f4ff99cbf9f7ebb3d0a9984
jaketrent <trent.jake@gmail.com>
2020-02-29 19:38:49 -0700

    separate stylesheet solution

---
+++ 03-stylesheet/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+import './NewsletterOverride.css'
+import Newsletter from './Newsletter.js'
+
+function App() {
+  return (
+    <div className="app__newsletter">
+      <Newsletter />
+    </div>
+  )
+}
+
+export default App
