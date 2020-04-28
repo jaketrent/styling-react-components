1756627e675fadbc71956dcb2f70f86178cfe2a0
jaketrent <trent.jake@gmail.com>
2020-04-28 06:58:05 -0600

    parcel bundler example

---
+++ 07-parcel-bundler/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the</h1>
+      <div className="title">
+        <h1>Webpack Example</h1>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
